f8a9d991aaec59d93d804d697612708e1da02a4d
cuter97 <aguscastets@gmail.com>
2023-06-24 19:30:44 -0300

    create products post and dto

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
 
-  findAll() {
-    return `This action returns all products`;
-  }
+    private readonly logger = new Logger('ProductsService')//"ProductsService" clase en donde voy a usar el logger para manejar errores
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
+    constructor(
+        @InjectRepository(Product)
+        private readonly productRepository: Repository<Product>,
+    ) { }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+    async create(createProductDto: CreateProductDto) {
+        try {
+            const product = this.productRepository.create(createProductDto); //crea la instancia (registro) del producto con sus propiedades. NO lo guarda en la base de datos
+            await this.productRepository.save(product)//lo guarda en la base de datos
+            return product
+        } catch (error) {
+            this.handleDBExceptions(error)
+        }
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+    findAll() {
+        return `This action returns all products`;
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} product`;
+    }
+
+    update(id: number, updateProductDto: UpdateProductDto) {
+        return `This action updates a #${id} product`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} product`;
+    }
+
+    private handleDBExceptions(error: any) {
+        if (error.code === '23505')
+            throw new BadRequestException(error.detail)
+
+        this.logger.error(error)
+        throw new InternalServerErrorException('Unexpected error, check logs')
+    }
+
 }
