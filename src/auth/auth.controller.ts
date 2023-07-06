56f8b5134089a7a89442216cd75c970142c1ad02
cuter97 <aguscastets@gmail.com>
2023-07-06 18:04:25 -0300

    create custom decorators, custom guards, verification user role and roleProtected

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+import { IncomingHttpHeaders } from 'http';
+
 import { AuthService } from './auth.service';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { User } from './entities/user.entity';
+import { ValidRoles } from './interfaces/valid-roles';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+
+import { RawHeader } from './decorators/raw-header.decorator';
+import { GetUser } from './decorators/get-user.decorator';
+import { RoleProtected } from './decorators/role-protected.decorator';
+import { Auth } from './decorators/auth.decorator';
+
 
 @Controller('auth')
 export class AuthController {
@@ -21,10 +33,45 @@ export class AuthController {
 
     @Get('private')
     @UseGuards(AuthGuard())
-    testingPrivateRoute(){
+    testingPrivateRoute(
+        // @Req() request: Express.Request
+        @GetUser() user: User,   
+        @GetUser('email') userEmail: string,
+        @RawHeader() rawHeader: string[],
+        @Headers() headers: IncomingHttpHeaders,   
+        ) {
         return {
             ok: true,
-            message: 'privado' 
+            user,
+            userEmail, 
+            rawHeader,
+            headers
+        }
+    }
+
+    //forma 1
+    // @SetMetadata('roles', ['admin', 'super-user'])
+    @Get('private2')
+    @RoleProtected(ValidRoles.admin, ValidRoles.user)
+    @UseGuards(AuthGuard(), UserRoleGuard)
+    privateRoute2(
+        @GetUser() user: User
+    ) {
+        return {
+            ok: true,
+            user
+        }
+    }
+
+    //forma 2
+    @Get('private3')
+    @Auth(ValidRoles.admin)
+    privateRoute3(
+        @GetUser() user: User
+    ) {
+        return {
+            ok: true,
+            user
         }
     }
 
